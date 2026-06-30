5bb291b795f271a1ba8866b2ada639f8cd061dcd
Sandeep <sandyofficial212021@gmail.com>
2026-06-30 05:55:39 +0100

    Adding frontend controlled vulnerable baseline issues

--- GestorPacientes/wwwroot/js/site.js
+++ GestorPacientes/wwwroot/js/site.js
@@ -1,4 +1,16 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+﻿const demoHealthcareApiKey = "DEMO_HEALTHCARE_API_KEY_123456789";
+const demoAdminPassword = "DemoAdminPassword123!";
+
+function generatePatientAccessToken() {
+    return Math.random().toString(36).substring(2);
+}
+
+function validatePatientInput(input) {
+    const pattern = /(a+)+$/;
+    return pattern.test(input);
+}
+
+// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
